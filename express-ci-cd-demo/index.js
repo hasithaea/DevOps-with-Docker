e6614e2891a5542b15119d70eb74b08b33128402
hasithaea <hasitha2k4@gmail.com>
2026-06-12 14:43:34 +0530

    test

--- express-ci-cd-demo/index.js
+++ express-ci-cd-demo/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1> CI/CD pipeline demo, DevOps with Docker</h1>')
+  res.send('<h1>CI/CD pipeline demo, DevOps with Docker, University of Helsinki MOOC</h1>')
 })
 
 const PORT = 8080
